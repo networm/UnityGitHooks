a1a969daa1e5015a53702a777b407b318bd80adc
networm <zzjbcn@gmail.com>
2016-06-26 14:59:35 +0800

    Add testcase

---
+++ hooks/testcase.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf Assets
+mkdir -p Assets/Resources/Sounds/Test
+touch Assets/Resources.meta
+touch Assets/Resources/Sounds.meta
+touch Assets/Resources/Sounds/Test.meta
+touch Assets/Resources/keep.txt
